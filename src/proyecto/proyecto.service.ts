6f9fa93aa9be34ca73aef336e8961f37ac465d79
Luisfetoga2 <l.torresg@uniandes.edu.co>
2024-05-23 10:15:22 -0500

    proyecto logica

---
+++ src/proyecto/proyecto.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProyectoEntity } from './proyecto.entity/proyecto.entity';
+import { BusinessError, BusinessLogicException } from '../shared/errors/business-errors';
+
+@Injectable()
+export class ProyectoService {
+    constructor (
+        @InjectRepository(ProyectoEntity)
+        private readonly proyectoRepository: Repository<ProyectoEntity>
+    ) {}
+
+    async create(proyecto: ProyectoEntity): Promise<ProyectoEntity> {
+        if (proyecto.fechaFin <= proyecto.fechaInicio)
+            throw new BusinessLogicException('La fecha de fin debe ser posterior a la fecha de inicio', BusinessError.PRECONDITION_FAILED);
+        return await this.proyectoRepository.save(proyecto);
+    }
+}
